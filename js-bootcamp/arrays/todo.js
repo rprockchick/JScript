f00853aea5e5ae0f6062f023e44175f8e69b81c1
rprocks <raj.neetu@gmail.com>
2018-11-10 02:55:31 +0530

    test

---
+++ js-bootcamp/arrays/todo.js
@@ -0,0 +1,7 @@
+let todo = ['brush teeth','learn a lot','sleep on time','drink less','smoke less']
+console.log(`You have ${todo.length} todo items`)
+todo.push('get married')
+for(let i=0;i<todo.length;i++){
+    console.log(`${todo[i]}\n`)
+}
+
